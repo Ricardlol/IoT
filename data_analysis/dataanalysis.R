06963d9f05a109fe2c461ed007c63106b50bf2b9
isotopi <isoto3145@gmail.com>
2022-12-20 17:41:05 +0100

    some plots

--- data_analysis/dataanalysis.R
+++ data_analysis/dataanalysis.R
@@ -13,28 +13,31 @@ df <- df[,c("phone_number", "age", "gender", "full_name", "sensors.name", "senso
 ## decided to generate random data following a normal distribution with mean of 120 and a standard deviation of 40
 ### ref: https://www.healthline.com/health/diabetes/blood-sugar-level-chart#recommended-ranges
 sim_gc <- rnorm(1000, mean = 120, sd = 40)
+hist(sim_gc, xlab = "Glucose levels", main = "Histogram of Glucose levels")
 
 ## blood pressure
 ## decided to generate random data following a normal distribution with mean of 110 and a standard deviation of 15
 ### ref: https://www.emedicinehealth.com/what_is_a_normal_blood_pressure_range_by_age/article_em.htm
 sim_bp <- rnorm(1000, mean = 110, sd = 15)
+hist(sim_bp, xlab = "Blood pressure", main = "Histogram of Blood pressure")
 
 ## heart rate
 ## decided to generate random data following a normal distribution with mean of 40 and a standard deviation of 10
 ### ref: https://www.whoop.com/thelocker/normal-hrv-range-age-gender/
 sim_hr <- rnorm(1000, mean = 40, sd = 10)
+hist(sim_hr, xlab = "Heart rate", main = "Histogram of Heart rate")
 
 ## oxygen in blood
 ## decided to generate random data following a normal distribution with mean of 96 and a standard deviation of 3
 ### ref: https://www.emedicinehealth.com/what_is_a_good_oxygen_rate_by_age/article_em.htm
 sim_ob <- rnorm(1000, mean = 96, sd = 3)
-
+hist(sim_ob, xlab = "Oxygen in blood", main = "Histogram of Oxygen in blood")
 
 
 # let's create a data set with the generated data as a replacement for the historic of the patient that we don't have
 historic <- data.frame(
   "glucose levels (mg/dl)" = sim_gc
-  ,"blood presure (mm/Hg)" = sim_bp
+  ,"blood pressure (mm/Hg)" = sim_bp
   ,"heart rate (HRV)" = sim_hr
   ,"oxygen levels (HRV)" = sim_ob
   ,check.names=FALSE
@@ -45,7 +48,10 @@ historic <- data.frame(
 # between variables
 
 # for glucose levels and blood pressure
-test_gc <- cor.test(historic$`glucose levels (mg/dl)`, historic$`blood presure (mm/Hg)`)
+plot(historic$`glucose levels (mg/dl)`, historic$`blood pressure (mm/Hg)`,
+     xlab = "Glucose levels", ylab = "Blood pressure",
+     main = "Glcuose levels vs Blood pressure")
+test_gc <- cor.test(historic$`glucose levels (mg/dl)`, historic$`blood pressure (mm/Hg)`)
 print(test_gc)
 # The correlation coefficient between the two vectors turns out to be
 print(test_gc$estimate)
@@ -56,6 +62,9 @@ print(test_gc$p.value)
 # the glucose levels and the blood pressure
 
 # for glucose levels and heart rate
+plot(historic$`glucose levels (mg/dl)`, historic$`heart rate (HRV)`,
+     xlab = "Glucose levels", ylab = "Heart rate",
+     main = "Glcuose levels vs Heart rate")
 test_hr <- cor.test(historic$`glucose levels (mg/dl)`, historic$`heart rate (HRV)`)
 # The correlation coefficient between the two vectors turns out to be
 print(test_hr$estimate)
@@ -65,6 +74,9 @@ print(test_hr$p.value)
 # the glucose levels and the heart rate
 
 # for glucose levels and oxygen levels
+plot(historic$`glucose levels (mg/dl)`, historic$`oxygen levels (HRV)`,
+     xlab = "Glucose levels", ylab = "Oxygen in blood",
+     main = "Glcuose levels vs Oxygen in blood")
 test_ol <- cor.test(historic$`glucose levels (mg/dl)`, historic$`oxygen levels (HRV)`)
 # The correlation coefficient between the two vectors turns out to be
 print(test_ol$estimate)
@@ -98,8 +110,8 @@ print(c(low_bound_gc, X, up_bound_gc))
 if (df$sensors.data < low_bound_gc || df$sensors.data > up_bound_gc) print("You should check your glucose levels!")
 
 # confidence interval for blood pressure
-X <- mean(historic$`blood presure (mm/Hg)`)
-sd <- sd(historic$`blood presure (mm/Hg)`)
+X <- mean(historic$`blood pressure (mm/Hg)`)
+sd <- sd(historic$`blood pressure (mm/Hg)`)
 std_error <- sd / sqrt(n)
 margin_error <- t_score * std_error
 up_bound_bp <- X + margin_error
